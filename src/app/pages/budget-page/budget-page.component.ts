1d8e02bb590746840e283a6070faf434a77439c7
mat.poreda <mat.poreda@gmail.com>
2021-12-13 23:17:14 +0100

    Charts are working

--- src/app/pages/budget-page/budget-page.component.ts
+++ src/app/pages/budget-page/budget-page.component.ts
@@ -4,7 +4,7 @@ import Category from 'src/app/models/category.model';
 import { NotifierService } from 'src/app/service/notifier.service';
 import { MatDialog } from '@angular/material/dialog';
 import { DataStorageService } from 'src/app/service/data-storage/data-storage.service';
-import { Subscription } from 'rxjs';
+import { Subject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-budget-page',
@@ -12,34 +12,31 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./budget-page.component.css']
 })
 export class BudgetPageComponent implements OnInit {
-  categories: Array<Category>;
-  category: Category;
   type: string;
   startDate: any;
   endDate: any;
+  update$: Subject<any> = new Subject();
   balance= [
-    {name:"Balance" , value:5000},
-    {name:"Income" , value:10000},
-    {name:"Expenses" , value:3000},
-    {name:"Deposit" , value:2000},
+    {name:"" , value:0},
+  ];
+  income= [
+    {name:"" , value:0},
+  ];
+  expense= [
+    {name:"" , value:0},
+  ];
+  deposit= [
+    {name:"" , value:0},
   ];
   
-  saleData = [
-    { name: "Mobiles", value: 105000 },
-    { name: "Laptop", value: 55000 },
-    { name: "AC", value: 15000 },
-    { name: "Headset", value: 150000 },
-    { name: "Fridge", value: 20000 }
-  ];
   userDetails = JSON.parse(localStorage.getItem('UserDetails') || '{}');
   notifierSubscription: Subscription = this.notifierService.subjectNotifier.subscribe(notified => {
     this.ngOnChanges();
   });
 
   constructor(private dataStorageService: DataStorageService, public dialog: MatDialog, private notifierService: NotifierService) { 
-    this.category = new Category(0, 0, "", "", "",0,0,0);
+
     this.type = "";
-    this.categories=[];
     this.userDetails= JSON.parse(localStorage.getItem('UserDetails') || '{}');
     this.startDate = localStorage.startDate;
     this.endDate = localStorage.endDate;
@@ -47,23 +44,65 @@ export class BudgetPageComponent implements OnInit {
 
   ngOnInit(): void {
     this.userDetails= JSON.parse(localStorage.getItem('UserDetails') || '{}');
-   ;
-      this.categories=this.categories.sort((a, b) => (a.sum<b.sum) ? 1 : -1);
       this.dataStorageService.getUserBalance(this.userDetails.id, this.startDate, this.endDate).subscribe(
         (balance) => {
-          console.log(balance);
           this.balance = balance;
-          console.log(this.balance);
+        }
+      );
+
+      this.dataStorageService.getCategoryStatsByType(this.userDetails.id, localStorage.startDate, localStorage.endDate, 'INCOME').subscribe(
+        (income) => {
+          console.log(income);
+          this.income = income;
+        }
+      );
+      this.dataStorageService.getCategoryStatsByType(this.userDetails.id, localStorage.startDate, localStorage.endDate, 'EXPENSE').subscribe(
+        (expense) => {
+          console.log(expense);
+          this.expense = expense;
+        }
+      );
+      this.dataStorageService.getCategoryStatsByType(this.userDetails.id, localStorage.startDate, localStorage.endDate, "DEPOSIT").subscribe(
+        (deposit) => {
+          console.log(deposit);
+          this.deposit = deposit;
         }
       );
 
   }
 
-  async ngOnChanges(): Promise<void> {
+
+  ngOnChanges(): void {
+    this.dataStorageService.getUserBalance(this.userDetails.id, localStorage.startDate, localStorage.endDate).subscribe(
+      (balance) => {
+        this.balance = balance;
+      }
+    );
+
+    this.dataStorageService.getCategoryStatsByType(this.userDetails.id, localStorage.startDate, localStorage.endDate, "INCOME").subscribe(
+      (income) => {
+        this.income = income;
+      }
+    );
+    this.dataStorageService.getCategoryStatsByType(this.userDetails.id, localStorage.startDate, localStorage.endDate, "EXPENSE").subscribe(
+      (expense) => {
+        this.expense = expense;
+      }
+    );
+    this.dataStorageService.getCategoryStatsByType(this.userDetails.id, localStorage.startDate, localStorage.endDate, "DEPOSIT").subscribe(
+      (deposit) => {
+        console.log(deposit);
+        this.deposit = deposit;
+      }
+    );
+    this.updateChart();
   }
 
 
   ngOnDestroy() {
     this.notifierSubscription.unsubscribe();
   }
+  updateChart(){
+    this.update$.next(true);
+}
 }
--- src/app/service/data-storage/data-storage.service.ts
+++ src/app/service/data-storage/data-storage.service.ts
@@ -57,10 +57,14 @@ export class DataStorageService {
       return this.http.delete<any>(this.transactionURL + transactionDTO.id);
     }
 
-
     getUserBalance(userId: number, startDate: string, endDate: string) : Observable<any> {
       let params = {startDate: startDate, endDate: endDate};
       return this.http.get<any>(this.reportURL +userId+'/balance', {params: params});
+    }
+
+    getCategoryStatsByType(userId: number, startDate: string, endDate: string, type: string) : Observable<any> {
+      let params = {startDate: startDate, endDate: endDate, type: type};
+      return this.http.get<any>(this.reportURL +userId+'/category_stats_by_type', {params: params});
     }
 
 
